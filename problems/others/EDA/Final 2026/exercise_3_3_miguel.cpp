a2b2796c650f93b03c2f80b69fb2092cf2594002
andr-migue <miguelzamora210405@gmail.com>
2026-05-28 22:11:34 -0400

    init 3.3

--- problems/others/EDA/Final 2026/exercise_3_3_miguel.cpp
+++ problems/others/EDA/Final 2026/exercise_3_3_miguel.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
+#include "../../../../structures/undirected_ponderaded_graph.hpp"
 
 using namespace std;
+using namespace structures;
 
+int solve(ponderaded_list_graph &graph, int n, int k)
+{
 
+}
 
 signed main()
 {
